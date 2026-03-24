85ddaf988398ebbfea3d0a87b0d5f5a9564a90d1
UIT - NPC - Đăng <tofucut3@outlook.com>
2026-03-24 13:58:54 +0000

    Updated app.py

--- vote/app.py
+++ vote/app.py
@@ -2,12 +2,11 @@ from flask import Flask, render_template, request, make_response, g
 from redis import Redis
 import os
 import socket
-import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Nắng")
+option_b = os.getenv('OPTION_B', "Mưa")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
@@ -23,17 +22,15 @@ def get_redis():
 
 @app.route("/", methods=['POST','GET'])
 def hello():
-    voter_id = request.cookies.get('voter_id')
-    if not voter_id:
-        voter_id = hex(random.getrandbits(64))[2:-1]
-
-    vote = None
+    voter_id = request.cookies.get('voter_id') or os.urandom(8).hex()
+    weather_report = None
 
     if request.method == 'POST':
         redis = get_redis()
-        vote = request.form['vote']
-        app.logger.info('Received vote for %s', vote)
-        data = json.dumps({'voter_id': voter_id, 'vote': vote})
+        weather_report = request.form['vote']
+        app.logger.info('Dự báo nhận được: %s', weather_report)
+        
+        data = json.dumps({'voter_id': voter_id, 'vote': weather_report})
         redis.rpush('votes', data)
 
     resp = make_response(render_template(
@@ -41,11 +38,10 @@ def hello():
         option_a=option_a,
         option_b=option_b,
         hostname=hostname,
-        vote=vote,
+        vote=weather_report,
     ))
     resp.set_cookie('voter_id', voter_id)
     return resp
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)

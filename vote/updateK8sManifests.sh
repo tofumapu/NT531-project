3095ba033b3b6871b6f1a1b282b9f3cec8d05836
UIT - NPC - Đăng <tofucut3@outlook.com>
2026-03-24 11:08:03 +0000

    Updated updateK8sManifests.sh

--- vote/updateK8sManifests.sh
+++ vote/updateK8sManifests.sh
@@ -12,21 +12,16 @@ REPO_URL="https://AZDO_PAT_REMOVED@dev.azure.com/tofucut3/votingApp/_git/votingA
 
 # Clone repo vào /tmp
 git clone "$REPO_URL" /tmp/temp_repo
-
-# Di chuyển vào thư mục đã clone
 cd /tmp/temp_repo
 
-# Cập nhật tag image trong file deployment YAML
-# Tìm dòng chứa "image:" và cập nhật tag mới
-sed -i "s|image:.*${SERVICE}.*|image: <ACR-REGISTRY-NAME>/${IMAGE_REPO}/${SERVICE}:${TAG}|g" \
-  k8s-specifications/${SERVICE}-deployment.yaml
+# Cập nhật image tag trong file deployment
+sed -i "s|image:.*${SERVICE}.*|image: tofuvotingappacr.azurecr.io/${IMAGE_REPO}/${SERVICE}:${TAG}|g" \
+    k8s-specifications/${SERVICE}-deployment.yaml
 
-# Stage và commit thay đổi
+git config user.email "pipeline@azure.com"
+git config user.name "Azure Pipeline"
 git add .
-git commit -m "Update Kubernetes manifest - ${SERVICE} image tag to ${TAG}"
-
-# Push lên Azure Repo
+git commit -m "Update ${SERVICE} image tag to ${TAG}"
 git push
 
-# Dọn dẹp
 rm -rf /tmp/temp_repo
